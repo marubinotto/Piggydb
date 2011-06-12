ef3601a7f77a46e4c2d964cee56ed59080fea0f6
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-06-13 00:26:53 +0900

    Code formatting

--- src/main/java/marubinotto/piggydb/external/jdbc/SequenceAdjuster.java
+++ src/main/java/marubinotto/piggydb/external/jdbc/SequenceAdjuster.java
@@ -7,16 +7,16 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public abstract class SequenceAdjuster {
 
 	protected JdbcTemplate jdbcTemplate;
-	
+
 	protected String tableName;
 	protected String columnName;
-	
+
 	public SequenceAdjuster() {
 	}
-	
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
@@ -29,17 +29,16 @@ public abstract class SequenceAdjuster {
 	public void setColumnName(String columnName) {
 		this.columnName = columnName;
 	}
-    
-    protected long getMaxValue() {
-    	Assert.Property.requireNotNull(jdbcTemplate, "jdbcTemplate");
-    	Assert.Property.requireNotNull(tableName, "tableName");
-    	Assert.Property.requireNotNull(columnName, "columnName");
-    	
-    	Long value = (Long)this.jdbcTemplate.queryForObject(
-			"select max(" + this.columnName + ") from " + this.tableName, 
-			Long.class);
-    	return value != null ? value : 0;
-    }
-    
-    public abstract long adjust() throws Exception;
+
+	protected long getMaxValue() {
+		Assert.Property.requireNotNull(jdbcTemplate, "jdbcTemplate");
+		Assert.Property.requireNotNull(tableName, "tableName");
+		Assert.Property.requireNotNull(columnName, "columnName");
+
+		Long value = (Long) this.jdbcTemplate.queryForObject("select max("
+			+ this.columnName + ") from " + this.tableName, Long.class);
+		return value != null ? value : 0;
+	}
+
+	public abstract long adjust() throws Exception;
 }
