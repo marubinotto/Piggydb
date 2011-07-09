e01dda784d7f5cecc49431647b6ec3efca10edc9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-07-10 08:25:09 +0900

    Working on Quick Edit: added FragmentContentEditor

--- src/main/java/marubinotto/util/CollectionUtils.java
+++ src/main/java/marubinotto/util/CollectionUtils.java
@@ -15,33 +15,33 @@ import ognl.Ognl;
 import ognl.OgnlException;
 
 public class CollectionUtils {
-	
-	public static <E> List<E> list(E ... elements) {
+
+	public static <E> List<E> list(E... elements) {
 		return new ArrayList<E>(Arrays.asList(elements));
 	}
 
-	public static <E> Set<E> set(E ... elements) {
+	public static <E> Set<E> set(E... elements) {
 		return new HashSet<E>(Arrays.asList(elements));
 	}
-	
+
 	public static <K, V> MapBuilder<K, V> map(K key, V value) {
-        return new MapBuilder<K, V>().map(key, value);
-    }
+		return new MapBuilder<K, V>().map(key, value);
+	}
 
-    public static class MapBuilder<K, V> extends HashMap<K, V> {
-        public MapBuilder() {
-        }
+	public static class MapBuilder<K, V> extends HashMap<K, V> {
+		public MapBuilder() {
+		}
 
-        public MapBuilder<K, V> map(K key, V value) {
-            put(key, value);
-            return this;
-        }
-    }
+		public MapBuilder<K, V> map(K key, V value) {
+			put(key, value);
+			return this;
+		}
+	}
 
-	public static <K,V> void pileValue(Map<K, List<V>> map, K key, V value) {
+	public static <K, V> void pileValue(Map<K, List<V>> map, K key, V value) {
 		Assert.Arg.notNull(map, "map");
 		Assert.Arg.notNull(value, "value");
-		
+
 		List<V> values = map.get(key);
 		if (values == null) {
 			values = new ArrayList<V>();
@@ -49,16 +49,16 @@ public class CollectionUtils {
 		}
 		values.add(value);
 	}
-	
+
 	public static <T> List<T> inReverseOrder(Collection<T> src) {
 		LinkedList<T> result = new LinkedList<T>();
-		for (T element : src) result.addFirst(element);
+		for (T element : src)
+			result.addFirst(element);
 		return result;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static <K, V> Map<K, V> makeMap(Collection<V> collection, String keyByOgnl) 
-	throws OgnlException {
+	public static <K, V> Map<K, V> makeMap(Collection<V> collection, String keyByOgnl) throws OgnlException {
 		Map<K, V> map = new LinkedHashMap<K, V>();
 		for (V value : collection) {
 			K key = (K)Ognl.getValue(keyByOgnl, value);
@@ -69,7 +69,7 @@ public class CollectionUtils {
 		}
 		return map;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <E> List<E> covariantCast(List<? extends E> list) {
 		return (List<E>)list;
--- src/test/java/marubinotto/piggydb/util/ExperimentalTest.java
+++ src/test/java/marubinotto/piggydb/util/ExperimentalTest.java
@@ -27,131 +27,130 @@ import org.apache.oro.text.regex.Perl5Matcher;
 import org.junit.Test;
 
 public class ExperimentalTest {
-    
-    private PatternCompiler compiler = new Perl5Compiler();
-    private PatternMatcher matcher = new Perl5Matcher();
 
-    @Test
-    public void filePathAndUrl() throws Exception {
-    	String userHome = System.getProperty("user.home");
-    	System.out.println("userHome: " + userHome);
-    	
-    	File userHomePath = new File(userHome);
-    	System.out.println("userHomePath: " + userHomePath);
-    	
-    	URI userHomeURI = userHomePath.toURI();
-    	System.out.println("userHomeURI: " + userHomeURI);
-    	
-    	File userHomePath2 = FileUtils.toFile(userHomeURI.toURL());
-    	System.out.println("userHomePath2: " + userHomePath2);
-    }
-    
-    @Test
-    public void fileToUrlWithEscaping() throws Exception {
-    	File file = new File("/my docs/file.txt");
-       	System.out.println("toURI: " + file.toURI());
-       	// System.out.println("toURL: " + file.toURL());
-    }
+	private PatternCompiler compiler = new Perl5Compiler();
+	private PatternMatcher matcher = new Perl5Matcher();
 
-    @Test
-    public void replaceClickMenuTitleByRegex() throws Exception {
-    	final PatternMatcher matcher = new Perl5Matcher();
-    	Pattern pattern = new Perl5Compiler().compile("(>.+<)");
-    	String result = RegexUtils.substitute(matcher, pattern, 1, new MatchProcessor() {
+	@Test
+	public void filePathAndUrl() throws Exception {
+		String userHome = System.getProperty("user.home");
+		System.out.println("userHome: " + userHome);
+
+		File userHomePath = new File(userHome);
+		System.out.println("userHomePath: " + userHomePath);
+
+		URI userHomeURI = userHomePath.toURI();
+		System.out.println("userHomeURI: " + userHomeURI);
+
+		File userHomePath2 = FileUtils.toFile(userHomeURI.toURL());
+		System.out.println("userHomePath2: " + userHomePath2);
+	}
+
+	@Test
+	public void fileToUrlWithEscaping() throws Exception {
+		File file = new File("/my docs/file.txt");
+		System.out.println("toURI: " + file.toURI());
+		// System.out.println("toURL: " + file.toURL());
+	}
+
+	@Test
+	public void replaceClickMenuTitleByRegex() throws Exception {
+		final PatternMatcher matcher = new Perl5Matcher();
+		Pattern pattern = new Perl5Compiler().compile("(>.+<)");
+		String result = RegexUtils.substitute(matcher, pattern, 1, new MatchProcessor() {
 			public String process(String match) {
 				String title = match.substring(1, match.length() - 1);
 				return ">(" + title + ")<";
 			}
-			
+
 		}, "<a href=\"/piggydb/new-tag.htm\">New Tag</a>");
-    	System.out.println(result);
-    }
-    
-    @Test
-    public void youtubeUrl() throws Exception {
-    	Pattern pattern = compiler.compile("youtube\\.com/watch\\?v=([^&\\s]+)");
-    	boolean result = this.matcher.contains(
-    		"http://jp.youtube.com/watch?v=iEL2grmKSnM&feature=related", pattern);
-    	assertTrue(result);
-    	
-    	MatchResult matchResult = matcher.getMatch();
-    	System.out.println(matchResult.group(1));
-    }
-    
-    @Test
-    public void tableRow() throws Exception {
-    	Pattern pattern = compiler.compile("^(\\|\\|).+(\\|\\|\\s*)$");
+		System.out.println(result);
+	}
+
+	@Test
+	public void youtubeUrl() throws Exception {
+		Pattern pattern = compiler.compile("youtube\\.com/watch\\?v=([^&\\s]+)");
+		boolean result = this.matcher.contains("http://jp.youtube.com/watch?v=iEL2grmKSnM&feature=related", pattern);
+		assertTrue(result);
+
+		MatchResult matchResult = matcher.getMatch();
+		System.out.println(matchResult.group(1));
+	}
+
+	@Test
+	public void tableRow() throws Exception {
+		Pattern pattern = compiler.compile("^(\\|\\|).+(\\|\\|\\s*)$");
 		boolean result = this.matcher.matches("||hoge||fuga||", pattern);
 		System.out.println(result);
-    }
-    
-    @Test
-    public void match() throws Exception {
-    	Pattern pattern = compiler.compile("^(hogehoge)$");
-    	boolean result = this.matcher.contains("hogehoge", pattern);
+	}
+
+	@Test
+	public void match() throws Exception {
+		Pattern pattern = compiler.compile("^(hogehoge)$");
+		boolean result = this.matcher.contains("hogehoge", pattern);
 		System.out.println(result);
-    }
-    
-    @Test
-    public void userHome() throws Exception {
-    	System.out.println("IS_OS_WINDOWS: " + SystemUtils.IS_OS_WINDOWS);
-    	System.out.println("user.home: " + System.getProperty("user.home"));
-    	System.out.println("HOME: " + System.getenv("HOME"));
-    	System.out.println("HOMEDRIVE: " + System.getenv("HOMEDRIVE"));
-    	System.out.println("HOMEPATH: " + System.getenv("HOMEPATH"));
-    	System.out.println("USERPROFILE: " + System.getenv("USERPROFILE"));
-    	System.out.println("ALLUSERSPROFILE: " + System.getenv("ALLUSERSPROFILE"));
-    	System.out.println("SYSTEMDRIVE: " + System.getenv("SYSTEMDRIVE"));
-    }
-    
-    private static Analyzer analyzer = new CJKAnalyzer();
-    
-    @Test
-    public void tokenize() throws Exception {
-    	String text = "Piggydb is an easy-to-use Web application for building a personal knowledge repository.";
-    	text = text + " Piggydbは個人向けの知識を管理するためのWebアプリケーションです。";
-    	
-    	// Standard
-    	System.out.println("Standard ----");
-    	StringTokenizer tokenizer = new StringTokenizer(text, " \t\n\r\f+\"*%&/()=?'!,.;:-_#@|^~`{}[]");
-        while (tokenizer.hasMoreTokens()) {
-        	System.out.println("  word: " + tokenizer.nextToken());
-        }
-        
-        // N-gram
-        System.out.println("N-gram ----");
-        NGramTokenizer nGramTokenizer = new NGramTokenizer(new StringReader(text), 2, 2);
-        Token token = new Token();
-        while ((token = nGramTokenizer.next(token)) != null) {
-        	System.out.println("  word: " + token.term());
-        }
-        
-        // CJKAnalyzer
-        System.out.println("CJKAnalyzer ----");
-        TokenStream stream = analyzer.tokenStream("F", new StringReader( text ));
-        token = new Token();
-        while ((token = stream.next(token)) != null) {
-        	System.out.println("  word: " + token.term());
-        }
-    }
-    
-    @Test
-    public void sentenceIterator() throws Exception {
-    	printFirstSentence("");
-    	printFirstSentence("without a period");
-    	printFirstSentence("This is a pen. Hello world.");
-    	printFirstSentence("日本語です。どうですか？");
-    }
-    
-    private void printFirstSentence(String text) {
-    	BreakIterator iterator = BreakIterator.getSentenceInstance();
-    	iterator.setText(text);
-    	int end = iterator.next();
-    	if (end == BreakIterator.DONE) {
-    		System.out.println("DONE");
-    	}
-    	else {
-    		System.out.println(text.substring(0, end));
-    	}
-    }
+	}
+
+	@Test
+	public void userHome() throws Exception {
+		System.out.println("IS_OS_WINDOWS: " + SystemUtils.IS_OS_WINDOWS);
+		System.out.println("user.home: " + System.getProperty("user.home"));
+		System.out.println("HOME: " + System.getenv("HOME"));
+		System.out.println("HOMEDRIVE: " + System.getenv("HOMEDRIVE"));
+		System.out.println("HOMEPATH: " + System.getenv("HOMEPATH"));
+		System.out.println("USERPROFILE: " + System.getenv("USERPROFILE"));
+		System.out.println("ALLUSERSPROFILE: " + System.getenv("ALLUSERSPROFILE"));
+		System.out.println("SYSTEMDRIVE: " + System.getenv("SYSTEMDRIVE"));
+	}
+
+	private static Analyzer analyzer = new CJKAnalyzer();
+
+	@Test
+	public void tokenize() throws Exception {
+		String text = "Piggydb is an easy-to-use Web application for building a personal knowledge repository.";
+		text = text + " Piggydbは個人向けの知識を管理するためのWebアプリケーションです。";
+
+		// Standard
+		System.out.println("Standard ----");
+		StringTokenizer tokenizer = new StringTokenizer(text, " \t\n\r\f+\"*%&/()=?'!,.;:-_#@|^~`{}[]");
+		while (tokenizer.hasMoreTokens()) {
+			System.out.println("  word: " + tokenizer.nextToken());
+		}
+
+		// N-gram
+		System.out.println("N-gram ----");
+		NGramTokenizer nGramTokenizer = new NGramTokenizer(new StringReader(text), 2, 2);
+		Token token = new Token();
+		while ((token = nGramTokenizer.next(token)) != null) {
+			System.out.println("  word: " + token.term());
+		}
+
+		// CJKAnalyzer
+		System.out.println("CJKAnalyzer ----");
+		TokenStream stream = analyzer.tokenStream("F", new StringReader(text));
+		token = new Token();
+		while ((token = stream.next(token)) != null) {
+			System.out.println("  word: " + token.term());
+		}
+	}
+
+	@Test
+	public void sentenceIterator() throws Exception {
+		printFirstSentence("");
+		printFirstSentence("without a period");
+		printFirstSentence("This is a pen. Hello world.");
+		printFirstSentence("日本語です。どうですか？");
+	}
+
+	private void printFirstSentence(String text) {
+		BreakIterator iterator = BreakIterator.getSentenceInstance();
+		iterator.setText(text);
+		int end = iterator.next();
+		if (end == BreakIterator.DONE) {
+			System.out.println("DONE");
+		}
+		else {
+			System.out.println(text.substring(0, end));
+		}
+	}
 }
