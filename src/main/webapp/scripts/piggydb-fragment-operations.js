42d62406f58c28bfa03b5cfd2fa9b041975e408e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-07-19 20:52:35 +0900

    Refactoring JavaScript: introduced FragmentsView class

--- src/main/webapp/scripts/piggydb-fragment-operations.js
+++ src/main/webapp/scripts/piggydb-fragment-operations.js
@@ -122,6 +122,14 @@ Fragment.init = function() {
 Fragment.findInTheSameFragmentNode = function(node, selector) {
 	return jQuery(node).closest("table.fragment-node").find(selector);
 };
+Fragment.highlight = function(id, baseNode) {
+  var selector = ".fragment-header-" + id;
+  var color = "#ff9900";
+  if (baseNode == null)
+  	jQuery(selector).fadingHighlight(color);
+  else
+  	jQuery(baseNode).find(selector).fadingHighlight(color);
+};
 Fragment.prototype = {
 	id: function() {
 		return this.root.find("span.fragment-id:first").text();
@@ -533,4 +541,103 @@ var FragmentTree = {
     jQuery(".fragment-root-node .sortable-icon").remove();
   }
 };
+
+
+
+//
+// Fragments View 
+//
+function FragmentsView(id, fragmentsUrl, scale, orderBy, ascending) {
+	this.id = id;
+	this.rootDiv = jQuery("#" + id);
+	this.fragmentsUrl = fragmentsUrl;
+	this.scale = scale;
+	this.orderBy = orderBy;
+	this.ascending = ascending;
+	this.highlight = null;
+	this.highlighted = false;
+	this.contentDiv = this.rootDiv.find("div.view-content");
+	this.viewControl = this.rootDiv.find(".view-control");
+	this.pageIndex = 0;
+}
+FragmentsView.MIN_SCALE = 0;
+FragmentsView.MAX_SCALE = 1000;
+FragmentsView.prototype = {
+  init: function () {
+    var outer = this;
+    this.rootDiv.find(".view-slider").slider({ 
+      max: FragmentsView.MAX_SCALE,
+      min: FragmentsView.MIN_SCALE,
+      value: outer.scale,
+      change: function(event, ui) {
+        outer.scale = ui.value;
+        outer.loadFirstSet();
+      }
+    });
+    this.rootDiv.find(".select-orderBy").change(function () {
+      outer.orderBy = jQuery(this).val();
+      outer.loadFirstSet();
+    });
+    this.rootDiv.find(".select-ascending-or-not button").click(function () {
+      var button = jQuery(this);
+      if (button.hasClass("selected")) return;
+      button.siblings("button.selected").removeClass("selected");
+      button.addClass("selected");
+      
+      outer.ascending = (button.attr("name") == "ascending");
+      outer.loadFirstSet();
+    });
+    
+    this.loadFirstSet();
+  },
+
+  createParameters: function () {
+    return {
+      "viewId": this.id, 
+      "scale": this.scale, 
+      "orderBy": this.orderBy,
+      "ascending": this.ascending
+    };
+  },
+  
+  loadFirstSet: function () {
+  	this.pageIndex = 0;
+    this.contentDiv.empty();
+    var loadIcon = this.contentDiv.putLoadingIcon("margin: 5px;");
+    var outer = this;
+    jQuery.get(this.fragmentsUrl, this.createParameters(), function(html) {
+      if (jQuery.trim(html) != "") {
+      	outer.contentDiv.append(html);
+      	outer.viewControl.show();
+        prettyPrint();
+        if (outer.highlight != null && !outer.highlighted) {
+        	Fragment.highlight(outer.highlight, outer.contentDiv);
+          // highlighing should be done only once 
+        	// in other words, when a user changes the scale in the same page,
+          // highlighing should not be enabled.
+          outer.highlighted = true;
+        }
+      }
+      loadIcon.remove();
+    });
+  },
+  
+	showMore: function (button) {
+    jQuery(button).remove();
+    var fragmentsContainer = this.rootDiv.find(".fragments-container");
+    
+    var loadIcon = jQuery('<div style="text-align: center; margin-bottom: 5px;">')
+      .appendTo(fragmentsContainer);
+    loadIcon.putLoadingIcon(null);
+    
+    var params = this.createParameters();
+    params.pi = ++this.pageIndex;
+    jQuery.get(this.fragmentsUrl, params, function(html) {
+      fragmentsContainer.append(html);
+      loadIcon.remove();
+      prettyPrint();
+    });
+  }
+};
+
 
