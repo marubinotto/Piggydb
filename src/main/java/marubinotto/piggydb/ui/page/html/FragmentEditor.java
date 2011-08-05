76d1113ba5cf0a3cd6dc53dbb7e7dbc3b8441ad7
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-08-06 08:23:54 +0900

    Working on Quick Edit for title: maxlength

--- src/main/java/marubinotto/piggydb/ui/page/html/FragmentEditor.java
+++ src/main/java/marubinotto/piggydb/ui/page/html/FragmentEditor.java
@@ -1,5 +1,8 @@
 package marubinotto.piggydb.ui.page.html;
 
+import marubinotto.piggydb.model.Fragment;
+
 public class FragmentEditor extends AbstractSingleFragment {
 
+	public int titleMaxLength = Fragment.TITLE_MAX_LENGTH;
 }
