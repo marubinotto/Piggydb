774fdfcf356767a1c547907d28f64647374122b5
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-08-20 19:00:32 +0900

    Added a test case

--- src/test/java/marubinotto/piggydb/model/fragment/TagRoleTest.java
+++ src/test/java/marubinotto/piggydb/model/fragment/TagRoleTest.java
@@ -1,36 +1,44 @@
 package marubinotto.piggydb.model.fragment;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Enclosed;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import marubinotto.piggydb.impl.InMemoryDatabase;
+import marubinotto.piggydb.model.FragmentRepository;
+import marubinotto.piggydb.model.Tag;
 import marubinotto.piggydb.model.TagRepository;
 import marubinotto.piggydb.model.User;
 import marubinotto.piggydb.model.entity.RawFragment;
 import marubinotto.piggydb.model.exception.InvalidTitleException;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Enclosed;
+
 @RunWith(Enclosed.class)
 public class TagRoleTest {
 	
-	public static class DefaultTest {
-
-		private RawFragment object = new RawFragment();
+	public static class TestBase {
 		
-		private User normalUser = new User("normal");
-		private TagRepository tagRepository = new InMemoryDatabase().getTagRepository();
+		protected FragmentRepository fragmentRepository = 
+			new InMemoryDatabase().getFragmentRepository();
+		protected TagRepository tagRepository = fragmentRepository.getTagRepository();
+		
+		protected User normalUser = new User("normal");
+	}
+	
+	public static class DefaultTest extends TestBase {
+
+		private RawFragment object = new RawFragment();		
 		
 		@Test
-		public void isNotTagByDefault() throws Exception {
+		public void isNotTag() throws Exception {
 			assertEquals(false, this.object.isTag());
 			assertNull(this.object.getTagId());
 			assertNull(this.object.asTag());
 		}
 		
 		@Test
-		public void validateDefaultState() throws Exception {
+		public void validate() throws Exception {
 			this.object.validateTagRole(this.normalUser, this.tagRepository);
 		}
 		
@@ -38,6 +46,18 @@ public class TagRoleTest {
 		public void newTagFragmentShouldHaveTitle() throws Exception {
 			this.object.setAsTagByUser(true, this.normalUser);
 			this.object.validateTagRole(this.normalUser, this.tagRepository);
+		}
+		
+		@Test
+		public void newTagRole() throws Exception {
+			this.object.setTitleByUser("test", this.normalUser);
+			this.object.setAsTagByUser(true, this.normalUser);
+			this.object.validateTagRole(this.normalUser, this.tagRepository);
+			
+			Tag tag = this.object.asTag();
+			
+			assertEquals("test", tag.getName());
+			assertEquals("normal", tag.getCreator());
 		}
 	}
 }
