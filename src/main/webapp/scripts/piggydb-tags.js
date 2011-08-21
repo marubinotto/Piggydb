14112d6f0859afa9cb049542b9d9947c60bd2ec7
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-08-21 14:23:26 +0900

    Deleted the feature of manipulating tags from TagsPage

--- src/main/webapp/scripts/piggydb-tags.js
+++ src/main/webapp/scripts/piggydb-tags.js
@@ -1,10 +1,4 @@
 /*
- * -event handler slot, which starts with "slotOn", should NOT depend on "this" context.
- * -event handler normally should NOT depend on "this" unless it is attached to a slot directory.
- */
-
-
-/*
  * TagUtils
  */
 var TagUtils = {
@@ -12,37 +6,11 @@ var TagUtils = {
     return '<span class="' + tagIconClass(tagName) + '">&nbsp;<\/span>';
   },
   
-  htmlTagIconSelected: function(tagName) {
-    return '<span class="' + tagIconClass(tagName) + ' tagIcon-selected">&nbsp;<\/span>';
-  },
-  
-  selectable: true,
-  
-  htmlTag: function(tag, isSelected, isDisabled) {
+  htmlTag: function(tag) {
     var html = [];
-    
-    // icon
-    if (isSelected) 
-      html.push(TagUtils.htmlTagIconSelected(tag.name));
-    else 
-      html.push(TagUtils.htmlTagIcon(tag.name));
-    
-    // checkbox
-    if (this.selectable) {
-      html.push('<input type="checkbox"');
-      if (isSelected) html.push(' checked="checked"');
-      if (isDisabled) html.push(' disabled="disabled"');
-      html.push(' class="tagCheckbox" name="selectedTags" value="' + tag.id + '"/>');
-    }
-    else {
-      html.push(" ");
-    }
-    
-    // name
-    html.push('<a class="tag' + (isSelected ? ' tag-selected' : '') + '"');
-    html.push(' href="tag.htm?id=' + tag.id + '">');
+    html.push(TagUtils.htmlTagIcon(tag.name));
+    html.push(' <a class="tag" href="tag.htm?id=' + tag.id + '">');
     html.push(tag.name + '<\/a>');
-
     return html.join("");
   }
 };
@@ -51,47 +19,11 @@ var TagUtils = {
 /*
  * TagView (base class)
  */
-function TagView(widget, tagData, nodeState, onTagCheckboxChange) {
+function TagView(widget, tagData) {
   this.widget = widget;
   this.tagData = tagData;
-  this.nodeState = nodeState;
-  this.onTagCheckboxChange = onTagCheckboxChange;
 }
 TagView.prototype = {
-  setSelection: function(tagId, checked) {
-    var checkboxes = this.widget.find('input.tagCheckbox[value=' + tagId + ']');
-    var tagIcon = checkboxes.siblings(".tagIcon").removeClass('tagIcon-selected');
-    var tagLink = checkboxes.siblings("a.tag").removeClass('tag-selected');
-    if (checked) {
-      checkboxes.attr("checked", "checked");
-      tagIcon.addClass("tagIcon-selected");
-      tagLink.addClass("tag-selected");
-    }
-    else {
-      checkboxes.removeAttr("checked");
-    } 
-  },
-  
-  disableCheckboxes: function(tagId2name) {
-    for (var tagId in tagId2name) {
-      this.widget.find('input.tagCheckbox[value=' + tagId + ']').attr("disabled", "disabled");
-    }
-  },
-  
-  clearAllSelection: function() {
-    this.widget.find('input.tagCheckbox').removeAttr("checked")
-      .siblings(".tagIcon").removeClass('tagIcon-selected').end()
-      .siblings("a.tag").removeClass('tag-selected');
-  },
-  
-  restoreSelections: function(tagId2name) {
-    this.clearAllSelection();
-    this.widget.find('input.tagCheckbox').removeAttr("disabled");
-    for (var tagId in tagId2name) {
-      this.setSelection(tagId, true);
-    }
-  },
-  
   loading: function() {
     this.widget.html('<img src="images/load.gif" border="0" style="margin:5px"/>');
   }
@@ -101,8 +33,8 @@ TagView.prototype = {
 /*
  * TagCloud
  */
-function TagCloud(widget, tagData, nodeState, onTagCheckboxChange) {
-  TagView.call(this, widget, tagData, nodeState, onTagCheckboxChange);
+function TagCloud(widget, tagData) {
+  TagView.call(this, widget, tagData);
 }
 TagCloud.prototype = jQuery.extend({
   render: function() {
@@ -111,34 +43,22 @@ TagCloud.prototype = jQuery.extend({
     var outer = this;
     this.tagData.tagsByPopularity(function(tags) {
       outer.widget.empty();
-      
-      if (tags.length == 0) return; 
+
+      if (tags.length == 0) return;
       
       var containerDiv = jQuery('<div class="container">');
       jQuery.each(tags, function(index, tag) {
-        outer.addTag(
-          tag, 
-          outer.nodeState.isSelected(tag.id),
-          outer.nodeState.isDisabled(tag.id),
-          containerDiv);
+        outer.addTag(tag, containerDiv);
       });
       outer.widget.append(containerDiv);
     });
   },
   
-  addTag: function(tag, isSelected, isDisabled, containerDiv) {
+  addTag: function(tag, containerDiv) {
     var html = ['<span class="tag" style="font-size: ' + tag.popularity + 'px;">'];
-    html.push(TagUtils.htmlTag(tag, isSelected, isDisabled));
+    html.push(TagUtils.htmlTag(tag));
     html.push("</span> <span> </span>");
-    
-    var outer = this;
-    var dom = jQuery(html.join(""));
-    dom.find('.tagCheckbox').click(function() {
-      outer.setSelection(tag.id, this.checked);
-      outer.onTagCheckboxChange(this, tag);
-    });  
-    
-    containerDiv.append(dom);
+    containerDiv.append(html.join(""));
   }
 }, TagView.prototype);
 
@@ -146,8 +66,8 @@ TagCloud.prototype = jQuery.extend({
 /*
  * TagTree
  */
-function TagTree(widget, tagData, nodeState, onTagCheckboxChange) {
-  TagView.call(this, widget, tagData, nodeState, onTagCheckboxChange);
+function TagTree(widget, tagData) {
+  TagView.call(this, widget, tagData);
 }
 TagTree.prototype = jQuery.extend({
   render: function() {
@@ -167,13 +87,7 @@ TagTree.prototype = jQuery.extend({
       node.slotOnChildrenRequired = function(id, li) { 
         outer.loadChildren(id, li); 
       };
-      node.slotOnCheckboxClicked = function(checkbox, tag) {
-        outer.setSelection(tag.id, checkbox.checked);
-        outer.onTagCheckboxChange(checkbox, tag);
-      };
-      ul.append(node.createDom(
-          outer.nodeState.isSelected(tag.id), 
-          outer.nodeState.isDisabled(tag.id)));
+      ul.append(node.createDom());
     });
   },
   
@@ -209,14 +123,14 @@ TagTreeNode.prototype = {
       return this.isLast ? "leafLastNode" : "leaf";
   }	,
   
-  createDom: function(isSelected, isDisabled) {
+  createDom: function() {
     // HTML
     var html = ['<li class="' + this.nodeType() + '">'];
     if (this.tag.hasChildren) {
       html.push('<a href="#" class="toggle">');
       html.push('<img src="style/tree/transparent.gif" border="0"/><\/a>&nbsp;');
     }
-    html.push(TagUtils.htmlTag(this.tag, isSelected, isDisabled));
+    html.push(TagUtils.htmlTag(this.tag));
     html.push('<\/li>');
     
     // create DOM
@@ -226,9 +140,6 @@ TagTreeNode.prototype = {
       outer.onToggleClicked(this);
       return false;
     });
-    dom.find('.tagCheckbox').click(function() {
-      outer.slotOnCheckboxClicked(this, outer.tag);
-    });  
     return dom;
   },
   
@@ -246,17 +157,15 @@ TagTreeNode.prototype = {
     }
   },
   
-  slotOnChildrenRequired: function(id, li) {},
-  
-  slotOnCheckboxClicked: function(checkbox, tag) {}
+  slotOnChildrenRequired: function(id, li) {}
 };
 
 
 /*
  * TagFlat
  */
-function TagFlat(widget, tagData, nodeState, onTagCheckboxChange, messages) {
-  TagView.call(this, widget, tagData, nodeState, onTagCheckboxChange);
+function TagFlat(widget, tagData, messages) {
+  TagView.call(this, widget, tagData);
   this.pageIndex = 0;
   this.messages = messages;
 }
@@ -286,11 +195,7 @@ TagFlat.prototype = jQuery.extend({
           containerDiv = jQuery('<div class="container">').appendTo(outer.widget);
           if (tag.name.charCodeAt(0) > 127) others = true;
         }
-        outer.addTag(
-          tag, 
-          outer.nodeState.isSelected(tag.id),
-          outer.nodeState.isDisabled(tag.id),
-          containerDiv);
+        outer.addTag(tag, containerDiv);
       });
       
       if (pageCount > 1) {
@@ -299,19 +204,11 @@ TagFlat.prototype = jQuery.extend({
     });
   },
   
-  addTag: function(tag, isSelected, isDisabled, containerDiv) {
+  addTag: function(tag, containerDiv) {
     var html = ['<span class="tag">'];
-    html.push(TagUtils.htmlTag(tag, isSelected, isDisabled));
+    html.push(TagUtils.htmlTag(tag));
     html.push("</span> <span> </span>");
-    
-    var outer = this;
-    var dom = jQuery(html.join(""));
-    dom.find('.tagCheckbox').click(function() {
-      outer.setSelection(tag.id, this.checked);
-      outer.onTagCheckboxChange(this, tag);
-    });  
-    
-    containerDiv.append(dom);
+    containerDiv.append(jQuery(html.join("")));
   },
   
   htmlPageLink: function(pageIndex, pageCount) {
@@ -345,240 +242,4 @@ TagFlat.prototype = jQuery.extend({
     return dom;
   }
 }, TagView.prototype);
-
-
-/*
- * TagList
- */
-function TagList(ul) {
-  this.ul = ul;
-  this.id2name = {};
-}
-TagList.prototype = {
-  get: function(id) {
-    return this.id2name[id];
-  },
-  
-  addOrRemove: function(add, id, name) {
-    if (add) 
-      this.id2name[id] = name; 
-    else 
-      delete this.id2name[id];
-    this.updateView();
-  },
-  
-  clear: function() {
-    this.id2name = {};
-    this.updateView();
-  },
-    
-  updateView: function() {
-    var count = 0;
-    this.ul.empty();
-    for (var id in this.id2name) {
-      var html = ['<li>'];
-      html.push(TagUtils.htmlTagIconSelected(this.id2name[id]) + ' ');
-      if (isNaN(id)) {
-        html.push(this.id2name[id]);
-        html.push(' <span style="color: silver;">(new)<\/span>');
-      }
-      else {
-        html.push('<a class="tag" href="tag.htm?id=' + id  + '">');
-        html.push(this.id2name[id]);
-        html.push('<\/a>');
-      }
-      html.push(' <a class="delete-tag" href="#">');
-      html.push('<img src="images/delete.gif" /><\/a><\/li>');
-      var li = jQuery(html.join(""));
-      
-      // Set an event handler for the delete button
-      var tagList = this;
-      var createHandler = function(id) {
-        return function() {
-          tagList.onDeleteButtonClick(id);
-          return false;
-        }
-      };
-      li.find('a.delete-tag').click(createHandler(id));
-      
-      this.ul.append(li);
-      count++;
-    }
-    this.slotOnUpdate(count);
-  },
-  
-  slotOnUpdate: function(count) {},
-  
-  onDeleteButtonClick: function(id) {
-    this.addOrRemove(false, id, null);
-    this.slotOnTagDeletion(id);
-  },
-  
-  slotOnTagDeletion: function(tagId) {}
-};
-
-
-/*
- * TagSelection
- */
-function TagSelection(widget, deleteAllConfirmMessage) {
-  this.widget = widget;
-  this.deleteAllConfirmMessage = deleteAllConfirmMessage;
-  this.selectedTags = new TagList(this.widget.find('div.selected-tags ul.tags'));
-  this.classifyingTags = new TagList(this.widget.find('div.classified-by ul.tags'));
-  
-  // event handlers
-  var outer = this;
-  this.widget.find("#create-filter").click(function() { 
-    outer.onCreateFilterClick();
-  });
-  this.widget.find("#set-classification-mode").click(function() { 
-    outer.onSetClassificationModeClick();
-  });
-  this.widget.find("#delete-all").click(function() { 
-    outer.onDeleteAllClick();
-  });
-  this.widget.find("#back-to-selection").click(function() { 
-    outer.onBackToSelectionClick();
-  });
-  this.widget.find("#add-classifying-tag").click(function() { 
-    outer.onAddClassifyingTagClick();
-  });
-  this.widget.find("#classify-all").click(function() { 
-    outer.onClassifyAllClick();
-  });
-  this.selectedTags.slotOnUpdate = function(count) {
-    if (count > 0)
-      outer.widget.show();
-    else
-      outer.widget.fadeOut();
-  };
-}
-TagSelection.prototype = {
-  setHandlerOnTagDeletion: function(onTagDeletion) {
-    this.selectedTags.slotOnTagDeletion = onTagDeletion;
-    this.classifyingTags.slotOnTagDeletion = onTagDeletion;
-  },
-  
-  isClassificationMode: function() {
-    return this.widget.find('#classified-by-pane').css("display") != "none";
-  },
-  
-  isSelected: function(tagId) {
-    return this.selectedTags.get(tagId) || 
-      (this.isClassificationMode() && this.classifyingTags.get(tagId));
-  },
-  
-  isDisabled: function(tagId) {
-    return this.selectedTags.get(tagId) && this.isClassificationMode();
-  },
-  
-  setSelection: function(selected, id, name) {
-    if (this.isClassificationMode()) {
-      this.classifyingTags.addOrRemove(selected, id, name);
-    }
-    else {
-      this.selectedTags.addOrRemove(selected, id, name);
-    }
-  },
-  
-  onCreateFilterClick: function() {
-    var form = jQuery('form[name=filter-form]');
-    form.empty();
-    
-    FormUtils.addParamToForm(form, "new", "true");
-    for (var tagId in this.selectedTags.id2name) {
-      FormUtils.addParamToForm(form, "tagIds", tagId);
-    }
-    form.submit();
-  },
-  
-  onSetClassificationModeClick: function() {
-    this.classifyingTags.clear();
-    
-    this.widget.find('#selected-tags-pane .section-title').css({ color:"silver" });
-    this.widget.find('div.selected-tags .delete-tag').hide();
-    this.widget.find('#buttons-for-selected-tags').hide(); 
-    this.widget.find('#classified-by-pane').show();
-    this.widget.find('#buttons-for-classification').show();
-   
-    this.slotOnClassificationModeSet(this.selectedTags.id2name);
-  },
-  
-  onDeleteAllClick: function() {
-    if (!window.confirm(this.deleteAllConfirmMessage)) return;
-    
-    var form = jQuery('form[name=tags-form]').empty();
-    FormUtils.addParamToForm(form, "command", "deleteAll");
-    for (var tagId in this.selectedTags.id2name) {
-      FormUtils.addParamToForm(form, "selectedTagIds", tagId);
-    }
-    form.submit();
-  },
-  
-  slotOnClassificationModeSet: function(selectedId2name) {},
-  
-  onBackToSelectionClick: function() {
-    this.widget.find('#classified-by-pane').hide();
-    this.widget.find('#buttons-for-classification').hide();
-    this.widget.find('div.selected-tags .delete-tag').show();
-    this.widget.find('#buttons-for-selected-tags').show();
-    this.widget.find('#selected-tags-pane .section-title').css({ color:"black" });
-    
-    this.slotOnBackToSelection(this.selectedTags.id2name);
-  },
-  
-  slotOnBackToSelection: function(selectedId2name) {},
-  
-  onAddClassifyingTagClick: function() {
-    // Input
-    var tagNameInput = this.widget.find('#classifying-tag-name');
-    var tagName = jQuery.trim(tagNameInput.val());
-    tagNameInput.val("");
-    if (tagName == null || tagName == "") return;
-    
-    // Check the name
-    var outer = this;
-    jQuery.post(
-      "command/get-tag-id.htm", 
-      { name: tagName }, 
-      function(tagId) {
-        // a new tag (no id)
-        if (jQuery.trim(tagId) == "") {
-          tagId = "new" + new Date().getTime();
-        }
-        // an existing tag
-        else {
-          if (outer.selectedTags.get(tagId)) return;
-          outer.slotOnTagSelection(tagId);
-        }
-        outer.setSelection(true, tagId, escapeHtml(tagName));
-      });
-  },
-  
-  slotOnTagSelection: function(tagId) {},
-  
-  onClassifyAllClick: function() {
-    var form = jQuery('form[name=tags-form]').empty();
-
-    FormUtils.addParamToForm(form, "command", "classifyAll");
-    
-    for (var tagId in this.selectedTags.id2name) {
-      FormUtils.addParamToForm(form, "selectedTagIds", tagId);
-    }
-
-    var count = 0;
-    for (var tagId in this.classifyingTags.id2name) {
-      count++;
-      if (isNaN(tagId)) {
-        FormUtils.addParamToForm(form, 
-          "classifyingTagNames", this.classifyingTags.id2name[tagId]);
-      }
-      else {
-        FormUtils.addParamToForm(form, "classifyingTagIds", tagId);
-      }
-    }
-    if (count > 0) form.submit();
-  }
-};
 
