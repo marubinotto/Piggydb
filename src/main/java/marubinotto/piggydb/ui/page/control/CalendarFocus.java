070cacd6b04d96c7c34bb8586a0674898221f639
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-08-28 22:03:01 +0900

    Code formatting

--- src/main/java/marubinotto/piggydb/ui/page/control/CalendarFocus.java
+++ src/main/java/marubinotto/piggydb/ui/page/control/CalendarFocus.java
@@ -5,42 +5,42 @@ import marubinotto.util.time.DateTime;
 import marubinotto.util.time.Interval;
 
 public class CalendarFocus {
-	
+
 	public static abstract class Type extends org.apache.commons.lang.enums.Enum {
-		
+
 		public static final Type MONTH = new Type("month") {
 			@Override
 			public Interval toInterval(DateTime dateTime) {
 				return dateTime.toMonth();
 			}
 		};
-		
+
 		public static final Type DAY = new Type("day") {
 			@Override
 			public Interval toInterval(DateTime dateTime) {
 				return dateTime.toDayInterval();
 			}
 		};
-		
+
 		private Type(String name) {
-	        super(name);
+			super(name);
 		}
-		
+
 		public abstract Interval toInterval(DateTime dateTime);
 	}
 
 	private Interval interval;
 	private Type type;
-	
+
 	public CalendarFocus() {
 	}
-	
+
 	public static final String FORMAT_MONTH = "yyyyMM";
 	public static final String FORMAT_DAY = "yyyyMMdd";
-	
+
 	public static CalendarFocus parseString(String string) {
 		if (string == null) return null;
-		
+
 		CalendarFocus focus = new CalendarFocus();
 		if (string.length() == FORMAT_MONTH.length()) {
 			focus.select(new DateTime(string, FORMAT_MONTH), Type.MONTH);
@@ -53,23 +53,23 @@ public class CalendarFocus {
 		}
 		return focus;
 	}
-	
+
 	public void select(DateTime dateTime, Type type) {
 		Assert.Arg.notNull(dateTime, "dateTime");
 		Assert.Arg.notNull(type, "type");
-		
+
 		this.interval = type.toInterval(dateTime);
 		this.type = type;
 	}
-	
+
 	public boolean isSelected(DateTime dateTime, Type type) {
 		Assert.Arg.notNull(dateTime, "dateTime");
 		Assert.Arg.notNull(type, "type");
-		
+
 		if (!type.equals(this.type)) return false;
 		return this.interval.containsInstant(dateTime);
 	}
-	
+
 	public Interval toInterval() {
 		return this.interval;
 	}
---
+++ src/main/java/marubinotto/piggydb/ui/page/html/TagPaletteCloud.java
@@ -0,0 +1,17 @@
+package marubinotto.piggydb.ui.page.html;
+
+public class TagPaletteCloud extends AbstractTagPalette {
+
+	private static final String VIEW_TYPE = "cloud";
+	
+	protected String getViewType() {
+		return VIEW_TYPE;
+	}
+
+	@Override 
+	protected void setModels() throws Exception {
+		super.setModels();
+		
+		// TODO
+	}
+}
