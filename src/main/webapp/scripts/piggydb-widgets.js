da8915279bd4bbb854b6d14b32c1f4d0b229b2f9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-12-20 20:34:32 +0900

    [New Fragment Form] added the content editor

--- src/main/webapp/scripts/piggydb-widgets.js
+++ src/main/webapp/scripts/piggydb-widgets.js
@@ -113,14 +113,36 @@ var FragmentFormDialog = {
 		jQuery("#dialog-fragment-form").remove();
 		jQuery.get("html/fragment-editor.htm", function(html) {
 			jQuery("body").append(html);
-			jQuery("#dialog-fragment-form").dialog({
-		    modal: true,
-		    width: 500,
+			var dialog = jQuery("#dialog-fragment-form");
+			FragmentFormDialog.init(dialog);
+			dialog.dialog({
+		    modal: false,
+		    width: 600,
 		    height: 400
 		  });
-			// TODO focus on the preview button
+			dialog.find("textarea.fragment-content").get(0).focus();
 		});
-	}
+	},
+	
+	init: function(dialog) {
+		jQuery.updnWatermark.attachAll();
+		dialog.find("input.fragment-as-tag").button({
+      icons: {
+      	primary: "ui-icon-piggydb-tag"
+      },
+      text: false
+	  });
+		dialog.find("textarea.fragment-content").markItUp(FragmentForm.markItUpSettings);
+		dialog.find(".markItUp .markItUpButton9 a").attr("href", constants["wiki-help-href"])
+	  	.click(FragmentFormDialog.onWikiHelpClick);
+	},
+  
+  wikiHelp: new Facebox("facebox-wiki-help"),
+  
+  onWikiHelpClick: function() {
+  	FragmentFormDialog.wikiHelp.show(this.href);
+    return false;
+  }
 };
 
 
