4d8b0263120fbaed13ff0df69c9ff5ada7de0dbb
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-12-23 00:35:48 +0900

    code formatting (つぶやくためにはコミットしなきゃいけない、これぞつぶやき駆動開発)

--- src/main/java/marubinotto/util/Size.java
+++ src/main/java/marubinotto/util/Size.java
@@ -4,81 +4,81 @@ import java.io.Serializable;
 import java.text.DecimalFormat;
 
 public class Size implements Serializable {
-	
-    private long value;
-    private String decimalFormat  = "0.00";
 
-    public Size(long value) {
-        this.value = value;
-    }
+	private long value;
+	private String decimalFormat = "0.00";
 
-    public long getValue() {
-        return value;
-    }
+	public Size(long value) {
+		this.value = value;
+	}
 
-    public double getAsGigaBytes() {
-        return ByteUnit.GIGA_BYTE.convert(this.value);
-    }
+	public long getValue() {
+		return value;
+	}
 
-    public String getAsGigaBytesString() {
-        DecimalFormat df = new DecimalFormat(this.decimalFormat);
-        return df.format(getAsGigaBytes()) + " "	 + ByteUnit.GIGA_BYTE;
-    }
+	public double getAsGigaBytes() {
+		return ByteUnit.GIGA_BYTE.convert(this.value);
+	}
 
-    public double getAsMegaBytes() {
-        return ByteUnit.MEGA_BYTE.convert(this.value);
-    }
+	public String getAsGigaBytesString() {
+		DecimalFormat df = new DecimalFormat(this.decimalFormat);
+		return df.format(getAsGigaBytes()) + " " + ByteUnit.GIGA_BYTE;
+	}
 
-    public String getAsMegaBytesString() {
-        DecimalFormat df = new DecimalFormat(this.decimalFormat);
-        return df.format(getAsMegaBytes()) + " " + ByteUnit.MEGA_BYTE;
-    }
-    
-    public double getAsKiroBytes() {
-    	return ByteUnit.KIRO_BYTE.convert(this.value);
-    }
-    
-    public String getAsKiroBytesString() {
-    	DecimalFormat df = new DecimalFormat(this.decimalFormat);
-        return df.format(getAsKiroBytes()) + " " + ByteUnit.KIRO_BYTE;
-    }
+	public double getAsMegaBytes() {
+		return ByteUnit.MEGA_BYTE.convert(this.value);
+	}
 
-    public String toString() {
-        if (this.value > ByteUnit.GIGA_BYTE.size) {
-            return getAsGigaBytesString();
-        }
-        else if (this.value > ByteUnit.MEGA_BYTE.size) {
-        	return getAsMegaBytesString();
-        }
-        else {
-            return getAsKiroBytesString();
-        }
-    }
+	public String getAsMegaBytesString() {
+		DecimalFormat df = new DecimalFormat(this.decimalFormat);
+		return df.format(getAsMegaBytes()) + " " + ByteUnit.MEGA_BYTE;
+	}
 
-    public static class ByteUnit {
-        public static final ByteUnit BYTE = new ByteUnit("byte", 1);
-        public static final ByteUnit KIRO_BYTE = new ByteUnit("KByte", 1024);
-        public static final ByteUnit MEGA_BYTE = new ByteUnit("MByte", 1048576);
-        public static final ByteUnit GIGA_BYTE = new ByteUnit("GByte", 1073741824);
+	public double getAsKiroBytes() {
+		return ByteUnit.KIRO_BYTE.convert(this.value);
+	}
 
-        private String name;
-        private long size;
+	public String getAsKiroBytesString() {
+		DecimalFormat df = new DecimalFormat(this.decimalFormat);
+		return df.format(getAsKiroBytes()) + " " + ByteUnit.KIRO_BYTE;
+	}
 
-        private ByteUnit(String name, long size) {
-            this.name = name;
-            this.size = size;
-        }
+	public String toString() {
+		if (this.value > ByteUnit.GIGA_BYTE.size) {
+			return getAsGigaBytesString();
+		}
+		else if (this.value > ByteUnit.MEGA_BYTE.size) {
+			return getAsMegaBytesString();
+		}
+		else {
+			return getAsKiroBytesString();
+		}
+	}
 
-        public long getSize() {
-            return size;
-        }
+	public static class ByteUnit {
+		public static final ByteUnit BYTE = new ByteUnit("byte", 1);
+		public static final ByteUnit KIRO_BYTE = new ByteUnit("KByte", 1024);
+		public static final ByteUnit MEGA_BYTE = new ByteUnit("MByte", 1048576);
+		public static final ByteUnit GIGA_BYTE = new ByteUnit("GByte", 1073741824);
 
-        public double convert(long byteSize) {
-            return (double)byteSize / (double)this.size;
-        }
+		private String name;
+		private long size;
 
-        public String toString() {
-            return this.name;
-        }
-    }
+		private ByteUnit(String name, long size) {
+			this.name = name;
+			this.size = size;
+		}
+
+		public long getSize() {
+			return size;
+		}
+
+		public double convert(long byteSize) {
+			return (double)byteSize / (double)this.size;
+		}
+
+		public String toString() {
+			return this.name;
+		}
+	}
 }
