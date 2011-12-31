cb7b92c1f819b1faf710fe742033ccfee2e565b8
Daisuke Morita <daisuke.marubinotto@gmail.com>
2011-12-31 19:34:09 +0900

    #study #javascript: added a simply-run-some-code module

---
+++ study-js/scripts/simply-run-some-code.js
@@ -0,0 +1,6 @@
+// This module must not clutter the global namespace.
+// put the code inside an anonymous function that is invoked immediately after being defined
+(function() {
+	var message = "this is a private variable.";
+	// alert(message);
+})();
