9924b38fc00681a71b2b426f5a699567a96fed3a
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-04-12 03:07:29 +0900

    #refactoring #javascript: deleted redundant code from FragmentForm

--- src/main/webapp/js/piggydb-fragments.js
+++ src/main/webapp/js/piggydb-fragments.js
@@ -28,9 +28,8 @@ jQuery(function() {
 var FragmentForm = {
 	init: function() {
 		jQuery('.content-type-switch input').click(FragmentForm.onContentTypeSwitch);
-	  jQuery("textarea.fragment-content").markItUp(FragmentForm.markItUpSettings);
-	  jQuery(".markItUp .markItUpButton9 a").attr("href", piggydb.server.wikiHelpUrl)
-	  	.click(FragmentForm.onWikiHelpClick);
+		piggydb.widget.FragmentForm.addToolBar(jQuery("textarea.fragment-content"));
+		piggydb.widget.FragmentForm.linkToWikiHelp(jQuery(".markItUp .markItUpButton9 a"));
 	  jQuery("input[name=preview]").click(function () {
 	    this.form.contentFieldHeight.value = jQuery(this.form.content).height();
 	  });
@@ -72,36 +71,6 @@ var FragmentForm = {
     jQuery('#' + formId + ' .for-' + oldValue).hide();
     jQuery('#' + formId + ' .for-' + newValue).show();
     this.form.contentType.value = newValue;
-  },
-  
-  wikiHelp: new piggydb.widget.Facebox("facebox-wiki-help"),
-  
-  onWikiHelpClick: function() {
-  	FragmentForm.wikiHelp.show(this.href);
-    return false;
-  },
-  
-  markItUpSettings: {
-    previewAutoRefresh: false,
-    previewParserPath:  '', // path to your Wiki parser
-    onShiftEnter:   {keepDefault:false, replaceWith:'\n\n'},
-    markupSet: [
-      {name: messages["editor-bold"], key: 'B', openWith: "'''", closeWith: "'''"}, 
-      {name: messages["editor-italic"], key: 'I', openWith: "''", closeWith: "''"}, 
-      {name: messages["editor-strike"], key: 'S', openWith: '__', closeWith: '__'}, 
-      {separator: '---------------' },
-      {name: messages["editor-bulleted-list"], openWith: '-'}, 
-      {name: messages["editor-numeric-list"], openWith: '+'}, 
-      {separator: '---------------' },
-      {name: messages["editor-link"], key: "L", openWith: "[[![URL:!:http://]!] ", 
-        closeWith: ']', placeHolder: messages["editor-link-label"] },
-      {name: messages["editor-embed-another-fragment"], key: 'E',
-        openWith: "fragment:[![" + messages["editor-fragment-id"] + "]!]:embed "},
-      {separator: '---------------' },
-      {name: messages["editor-quote"], openWith: '>', placeHolder: ''},
-      {separator: '---------------' },
-      {name: messages["help"]}
-    ]
   }
 };
 
