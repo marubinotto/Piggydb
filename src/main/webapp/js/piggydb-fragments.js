c98b029eecfaeca5c8c9c6ab75e1189bbe0895cf
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-04-12 03:44:23 +0900

    #refactoring #javascript: fragmentOps => piggydb.command

--- src/main/webapp/js/piggydb-fragments.js
+++ src/main/webapp/js/piggydb-fragments.js
@@ -21,7 +21,6 @@ jQuery(function() {
 });
 
 
-
 //
 // Fragment Form
 //
@@ -75,47 +74,48 @@ var FragmentForm = {
 };
 
 
-
-//
-// Fragment Operations
-//
-var fragmentOps = {
-  deleteRelation: function (id, relationHtml, relationContainerHtml) {
-    if (!window.confirm(messages["confirm-delete-relation"])) 
-      return false;
-    
-    piggydb.server.ajaxCommand("delete-relation", {"id": id});
-    relationHtml.fadeOut("slow", function() {
-      if (relationContainerHtml != null && relationHtml.siblings().size() == 0)
-        relationContainerHtml.remove();
-      else
-        relationHtml.remove();
-    });
-  },
-  
-  removeTag: function (fragmentId, tagName) {
-    if (!window.confirm(messages["confirm-remove-tag"] + ' : "' + tagName + '"')) 
-      return false;
-      
-    var fm = document.forms['removeTagForm'];
-    fm.fragmentId.value = fragmentId;
-    fm.tagName.value = tagName;
-    fm.submit();
-  },
-  
-  addTag: function (fragmentId, tagName) {
-    var fm = document.forms['addTagForm'];
-    fm.fragmentId.value = fragmentId;
-    fm.tagName.value = tagName;
-    fm.submit();
-  },
-  
-  removeBookmark: function(fragmentId) {
-    if (!window.confirm(messages["confirm-remove-bookmark"])) 
-      return false;
-      
-    var fm = document.forms['removeBookmarkForm'];
-    fm.fragmentId.value = fragmentId;
-    fm.submit();
-  }
-};
+piggydb.namespace("piggydb.command", (function() {
+	var _messages = piggydb.server.messages;
+	
+	return {
+		
+	  deleteRelation: function (id, relationHtml, relationContainerHtml) {
+	    if (!window.confirm(_messages["confirm-delete-relation"])) 
+	      return false;
+	    
+	    piggydb.server.ajaxCommand("delete-relation", {"id": id});
+	    relationHtml.fadeOut("slow", function() {
+	      if (relationContainerHtml != null && relationHtml.siblings().size() == 0)
+	        relationContainerHtml.remove();
+	      else
+	        relationHtml.remove();
+	    });
+	  },
+	  
+	  removeTag: function (fragmentId, tagName) {
+	    if (!window.confirm(_messages["confirm-remove-tag"] + ' : "' + tagName + '"')) 
+	      return false;
+	      
+	    var fm = document.forms['removeTagForm'];
+	    fm.fragmentId.value = fragmentId;
+	    fm.tagName.value = tagName;
+	    fm.submit();
+	  },
+	  
+	  addTag: function (fragmentId, tagName) {
+	    var fm = document.forms['addTagForm'];
+	    fm.fragmentId.value = fragmentId;
+	    fm.tagName.value = tagName;
+	    fm.submit();
+	  },
+	  
+	  removeBookmark: function(fragmentId) {
+	    if (!window.confirm(_messages["confirm-remove-bookmark"])) 
+	      return false;
+	      
+	    var fm = document.forms['removeBookmarkForm'];
+	    fm.fragmentId.value = fragmentId;
+	    fm.submit();
+	  }
+	};
+})());
