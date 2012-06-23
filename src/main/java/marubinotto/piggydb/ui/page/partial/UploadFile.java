5e3e084d4019facb6d2646a2eceb64fa5734c2bb
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-06-24 01:32:05 +0900

    #new_file_form: added marubinotto.piggydb.ui.page.partial.UploadFile.createUploadFilePath(String)

--- src/main/java/marubinotto/piggydb/ui/page/partial/UploadFile.java
+++ src/main/java/marubinotto/piggydb/ui/page/partial/UploadFile.java
@@ -1,5 +1,10 @@
 package marubinotto.piggydb.ui.page.partial;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import marubinotto.piggydb.ui.page.common.PageImports;
 
 public class UploadFile extends AbstractPartial {
@@ -9,5 +14,20 @@ public class UploadFile extends AbstractPartial {
 		super.setModels();
 		
 		addModel("jQueryPath", PageImports.JQUERY_PATH);
+		
+		getLogger().info(createUploadFilePath(".png"));
+	}
+	
+	private static final String UPLOAD_DIR_NAME = "upload";
+	
+	private File getUploadDir() throws IOException {
+		File dir = new File(getContext().getServletContext().getRealPath("/" + UPLOAD_DIR_NAME));
+		if (!dir.isDirectory()) FileUtils.forceMkdir(dir);
+		return dir;
+	}
+	
+	private File createUploadFilePath(String suffix) throws IOException {
+		String prefix = getContext().getSession().getId();
+		return File.createTempFile(prefix, suffix, getUploadDir());
 	}
 }
