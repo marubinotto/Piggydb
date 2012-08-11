bb1d34d9bb6b34aa63b8a2f014551c72ac2514e9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-08-12 05:05:43 +0900

    changed the path of the extension def file to 'META-INF/piggydb-extension-class'

--- src/main/java/marubinotto/piggydb/extension/Extension.java
+++ src/main/java/marubinotto/piggydb/extension/Extension.java
@@ -10,7 +10,7 @@ import java.util.Enumeration;
 // TODO user menu
 public abstract class Extension {
 	
-	public static final String DEF_FILE_NAME = "piggydb-extension-class";
+	public static final String DEF_FILE_NAME = "META-INF/piggydb-extension-class";
 
 	public static Enumeration<URL> allDefFiles() throws IOException {
 		return Extension.class.getClassLoader().getResources(DEF_FILE_NAME);
