013b200213013974183d130cbca5b1de1483a5f3
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-08-12 05:34:57 +0900

    updated the TODOs

--- src/main/java/marubinotto/piggydb/extension/Extension.java
+++ src/main/java/marubinotto/piggydb/extension/Extension.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
 
-// TODO static deployAllExtensions
-// TODO deploy
-// TODO register web files
+// TODO private static deployWebappFiles(ServletContext)
+// TODO public static deployAll(ServletContext)
+// TODO init(ServletContext)
 // TODO user menu
 public abstract class Extension {
 	
 	public static final String DEF_FILE_NAME = "META-INF/piggydb-extension-class";
+	public static final String WEBAPP_DIR = "META-INF/webapp";
 
 	public static Enumeration<URL> allDefFiles() throws IOException {
 		return Extension.class.getClassLoader().getResources(DEF_FILE_NAME);
