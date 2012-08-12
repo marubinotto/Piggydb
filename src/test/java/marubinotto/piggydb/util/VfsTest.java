b90b14081f2534ab9f60f179a9854fccdc0a8f8e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-08-13 00:07:54 +0900

    added a library 'Commons VFS'

---
+++ src/test/java/marubinotto/piggydb/util/VfsTest.java
@@ -0,0 +1,23 @@
+package marubinotto.piggydb.util;
+
+import java.net.URL;
+
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.VFS;
+import org.junit.Test;
+
+public class VfsTest {
+
+	@Test
+	public void vfsPackage() throws Exception {
+		URL url = getClass().getResource("/org/apache/commons/vfs2");
+		System.out.println(url);
+		
+		FileSystemManager fsManager = VFS.getManager();
+		FileObject dir = fsManager.resolveFile(url.toExternalForm());
+		for (FileObject file : dir.getChildren()) {
+			System.out.println(file.getName().getBaseName());
+		}
+	}
+}
