ae706285b6ec45ed6a08d4521c27b23b41cc83ad
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-08-13 07:16:45 +0900

    #extension: added initialization logic

--- src/main/java/marubinotto/piggydb/extension/Extension.java
+++ src/main/java/marubinotto/piggydb/extension/Extension.java
@@ -6,6 +6,7 @@ import java.util.Enumeration;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.vfs2.AllFileSelector;
@@ -16,8 +17,6 @@ import org.springframework.context.ApplicationContext;
 
 import marubinotto.util.Assert;
 
-// TODO init(ServletContext, ApplicationContext)
-// TODO user menu
 public abstract class Extension {
 	
 	private static Log logger = LogFactory.getLog(Extension.class);
@@ -53,6 +52,37 @@ public abstract class Extension {
 		ApplicationContext appContext) 
 	throws IOException {
 		Assert.Arg.notNull(servletContext, "servletContext");
+		
+		// deploy webapp files
 		deployWebappFiles(servletContext);
+		
+		// initialize extensions
+		for (Enumeration<URL> files = allDefFiles(); files.hasMoreElements();) {
+			URL defFile = files.nextElement();
+			try {
+				initExtension(defFile, servletContext, appContext);
+			}
+			catch (Exception e) {
+				logger.error("Extension initialization error: " + defFile.toExternalForm());
+			}
+		}
+	}
+	
+	private static void initExtension(
+		URL defFile, 
+		ServletContext servletContext,
+		ApplicationContext appContext) 
+	throws Exception {
+		String className = IOUtils.toString(defFile).trim();
+		logger.info("Initializing extension: " + className);
+		Extension extension = (Extension)Class.forName(className).newInstance();
+		extension.init(servletContext, appContext);
+	}
+	
+	public void init(
+		ServletContext servletContext,
+		ApplicationContext appContext) 
+	throws Exception {
+		// Do nothing by default
 	}
 }
