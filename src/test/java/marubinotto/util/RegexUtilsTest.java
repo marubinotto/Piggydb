74a2876d6e7ffc1d7bea2231d9487304a0ef8a6a
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-08-17 09:13:31 +0900

    code formatting

--- src/test/java/marubinotto/util/RegexUtilsTest.java
+++ src/test/java/marubinotto/util/RegexUtilsTest.java
@@ -11,35 +11,37 @@ import org.junit.Test;
 
 public class RegexUtilsTest {
 
-    static class Bracket implements RegexUtils.MatchProcessor {
-        public String process(String match) {
-            return "(" + match + ")";
-        }       
-    }
-      
-    private PatternCompiler compiler = new Perl5Compiler();
-    private PatternMatcher matcher = new Perl5Matcher();
+	static class Bracket implements RegexUtils.MatchProcessor {
+		public String process(String match) {
+			return "(" + match + ")";
+		}
+	}
 
-    @Test
-    public void shouldProcessMatchedTokenByMatchProcessor() throws Exception {
-        String input = "foo10hoge45huga";
-        String pattern = "\\d+";
-        String result = RegexUtils.substitute(
-            this.matcher, this.compiler.compile(pattern), 0, new Bracket(), input);
-        assertEquals("foo(10)hoge(45)huga", result);
-    }
-    
-    @Test
-    public void escapeMetacharsForJava() throws Exception {
-    	assertTrue("fo]o(b\\ar".matches(RegexUtils.escapeRegex("fo]o(b\\ar")));
-    }
-    
-    @Test
-    public void escapeMetacharsForOro() throws Exception {
-    	String input = "foo fo]o(b\\ar bar";
-        String pattern = RegexUtils.escapeRegex("fo]o(b\\ar");
-        String result = RegexUtils.substitute(
-            this.matcher, this.compiler.compile(pattern), 0, new Bracket(), input);
-        assertEquals("foo (fo]o(b\\ar) bar", result);
-    }
+	private PatternCompiler compiler = new Perl5Compiler();
+	private PatternMatcher matcher = new Perl5Matcher();
+
+	@Test
+	public void shouldProcessMatchedTokenByMatchProcessor() throws Exception {
+		String input = "foo10hoge45huga";
+		String pattern = "\\d+";
+		String result = 
+			RegexUtils.substitute(
+				this.matcher, this.compiler.compile(pattern), 0, new Bracket(), input);
+		assertEquals("foo(10)hoge(45)huga", result);
+	}
+
+	@Test
+	public void escapeMetacharsForJava() throws Exception {
+		assertTrue("fo]o(b\\ar".matches(RegexUtils.escapeRegex("fo]o(b\\ar")));
+	}
+
+	@Test
+	public void escapeMetacharsForOro() throws Exception {
+		String input = "foo fo]o(b\\ar bar";
+		String pattern = RegexUtils.escapeRegex("fo]o(b\\ar");
+		String result = 
+			RegexUtils.substitute(
+				this.matcher, this.compiler.compile(pattern), 0, new Bracket(), input);
+		assertEquals("foo (fo]o(b\\ar) bar", result);
+	}
 }
