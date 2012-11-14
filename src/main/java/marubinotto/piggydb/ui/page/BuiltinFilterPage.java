665461c9943812cef61bd2ba28ba5374a473d09b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2012-11-14 10:39:15 +0900

    #builtin_filter: updated BuiltinFilterPage to display the filtered fragments

--- src/main/java/marubinotto/piggydb/ui/page/BuiltinFilterPage.java
+++ src/main/java/marubinotto/piggydb/ui/page/BuiltinFilterPage.java
@@ -1,8 +1,20 @@
 package marubinotto.piggydb.ui.page;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
 import marubinotto.piggydb.ui.page.common.AbstractFragmentsPage;
+import marubinotto.piggydb.ui.page.partial.FragmentsByBuiltinFilter;
 
 public class BuiltinFilterPage extends AbstractFragmentsPage {
 
 	public String name;
+	
+	@Override
+	protected boolean onPreInit() throws Exception {	
+		if (isBlank(this.name) || 
+			!FragmentsByBuiltinFilter.queryTypes.containsKey(this.name)) {
+			setRedirect(HomePage.class);
+			return false;
+		}
+		return true;
+	}
 }
