88e7a51bd40382bd30479837014976ca28292794
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-02-03 20:16:39 +0900

    updated the build setting

--- src/test/java/marubinotto/util/UtilsTest.java
+++ src/test/java/marubinotto/util/UtilsTest.java
@@ -9,17 +9,17 @@ import org.junit.Test;
 
 public class UtilsTest {
 
-	@Test
-	public void getRandomWithExclusion() throws Exception {
-		List<Integer> results = new ArrayList<Integer>();
-		TreeSet<Integer> excludes = new TreeSet<Integer>();
-		int size = 10;
-		Random random = new Random();
-		for (int i = 0; i < size; i++) {
-			int picked = Utils.getRandomWithExclusion(random, size, excludes);
-			excludes.add(picked);
-			results.add(picked);
-		}
-		System.out.println(results);
-	}
+  @Test
+  public void getRandomWithExclusion() throws Exception {
+    List<Integer> results = new ArrayList<Integer>();
+    TreeSet<Integer> excludes = new TreeSet<Integer>();
+    int size = 10;
+    Random random = new Random();
+    for (int i = 0; i < size; i++) {
+      int picked = Utils.getRandomWithExclusion(random, size, excludes);
+      excludes.add(picked);
+      results.add(picked);
+    }
+    System.out.println(results);
+  }
 }
