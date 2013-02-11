1fa5216f29021a5e8eff7329f0156801fb56dfbd
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-02-11 22:46:38 +0900

    #smart_layout: fixed improper container positioning after switching from scrolled vertical layout

--- src/main/webapp/js/piggydb.widget.SmartLayout.js
+++ src/main/webapp/js/piggydb.widget.SmartLayout.js
@@ -37,9 +37,12 @@
 	
 	_object.setHorizontalLayout = function() {
 		if (_object.vertical) {
-			_object.vertical = false
-			_pageContent.toggleClass("sl-horizontal", true);
-			jQuery("body").css("overflow", "hidden");
+			// to make sure that the layout switching will be done with the scrollTop zero
+			jQuery('html,body').animate({ scrollTop: 0 }, 0, 'linear', function() {
+				_object.vertical = false
+				_pageContent.toggleClass("sl-horizontal", true);
+				jQuery("body").css("overflow", "hidden");
+			});
 		}
 		_pane.css("width", (_container.width() / 2) - 25);
 	};
