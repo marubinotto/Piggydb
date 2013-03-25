ecb7184f06e595ec443d9bb8a82610396287d89f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-03-25 16:33:52 +0900

    added DocumentViewUrlFilter to implement short URL for Document View

---
+++ src/main/java/marubinotto/piggydb/ui/DocumentViewUrlFilter.java
@@ -0,0 +1,57 @@
+package marubinotto.piggydb.ui;
+
+import static org.apache.commons.lang.StringUtils.removeStart;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DocumentViewUrlFilter implements Filter {
+  
+  private static Log logger = LogFactory.getLog(DocumentViewUrlFilter.class);
+
+  public void init(FilterConfig config) throws ServletException {
+  }
+  
+  public static final String PATH_PREFIX = "/d/";
+
+  public void doFilter(
+    ServletRequest request, 
+    ServletResponse response, 
+    FilterChain chain) 
+  throws IOException, ServletException {
+    // Get the request path
+    HttpServletRequest httpRequest = (HttpServletRequest)request;
+    String path = removeStart(httpRequest.getRequestURI(), httpRequest.getContextPath() + PATH_PREFIX);
+    logger.info("path: " + path);
+    
+    // Get the fragment ID from the path
+    Long fragmentId;
+    try {
+      fragmentId = StringUtils.isNotBlank(path) ? Long.parseLong(path) : 0L;
+    }
+    catch (NumberFormatException e) {
+      logger.info("invalid path: " + path);
+      return;
+    }
+    logger.info("fragmentId: " + fragmentId);
+
+    // Dispatch to its Document View
+    String docViewPath = "/document-view.htm";
+    if (fragmentId > 0) docViewPath += "?id=" + fragmentId;
+    httpRequest.getRequestDispatcher(docViewPath).forward(request, response);
+  }
+
+  public void destroy() {
+  }
+}
