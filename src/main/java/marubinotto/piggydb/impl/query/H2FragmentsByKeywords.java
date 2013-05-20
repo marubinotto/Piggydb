2d5fa5779ac149cad201ff940d109d99385bb7e1
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-05-20 19:13:45 +0900

    trivial refactoring

--- src/main/java/marubinotto/piggydb/impl/query/H2FragmentsByKeywords.java
+++ src/main/java/marubinotto/piggydb/impl/query/H2FragmentsByKeywords.java
@@ -1,10 +1,10 @@
 package marubinotto.piggydb.impl.query;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+
 import java.util.List;
 
 import marubinotto.piggydb.model.query.FragmentsByKeywords;
-
-import org.apache.commons.lang.StringUtils;
 
 public class H2FragmentsByKeywords 
 extends H2FragmentsQueryBase implements FragmentsByKeywords {
@@ -16,16 +16,8 @@ extends H2FragmentsQueryBase implements FragmentsByKeywords {
 	}
 	
 	protected void appendFromWhere(StringBuilder sql, List<Object> args) throws Exception {
-	  appendFromWhereForKeywordSearch(sql, args, this.keywords);
-	}
-	
-	public static void appendFromWhereForKeywordSearch(
-	  StringBuilder sql, 
-	  List<Object> args, 
-	  String keywords) 
-	throws Exception {
 	  sql.append("from fragment");
-    if (StringUtils.isNotBlank(keywords)) {
+    if (isNotBlank(this.keywords)) {
       sql.append(", FT_SEARCH_DATA(?, 0, 0) ft");
       sql.append(" where ft.TABLE ='FRAGMENT' and fragment.fragment_id = ft.KEYS[0]");
       args.add(keywords);
