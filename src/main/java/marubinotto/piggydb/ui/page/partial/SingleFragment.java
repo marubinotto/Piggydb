2dc3724169b50bcb4a1d863934a9b5044af04fd5
Daisuke Morita <daisuke.marubinotto@gmail.com>
2013-09-27 19:19:11 +0900

    #fragment-quick-view: added marubinotto.piggydb.ui.page.partial.SingleFragment

---
+++ src/main/java/marubinotto/piggydb/ui/page/partial/SingleFragment.java
@@ -0,0 +1,9 @@
+package marubinotto.piggydb.ui.page.partial;
+
+public class SingleFragment extends AbstractSingleFragment {
+
+  @Override 
+  protected boolean fetchesRelations() {
+    return true;
+  }
+}
