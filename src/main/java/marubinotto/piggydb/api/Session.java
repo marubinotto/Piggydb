21f6c8140baba3c04448063f03068eaefc7876e3
Daisuke Morita <daisuke.marubinotto@gmail.com>
2014-05-14 16:33:16 +0900

    added session persistence features to marubinotto.piggydb.api.Session

--- src/main/java/marubinotto/piggydb/api/Session.java
+++ src/main/java/marubinotto/piggydb/api/Session.java
@@ -1,6 +1,11 @@
 package marubinotto.piggydb.api;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import marubinotto.util.time.DateTime;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,8 +15,57 @@ public class Session {
   private static Log logger = LogFactory.getLog(Session.class);
   
   private HttpServletRequest request;
+  private HttpServletResponse response;
   
-  public Session(HttpServletRequest request) {
+  public Session(HttpServletRequest request, HttpServletResponse response) {
     this.request = request;
+    this.response = response;
+  }
+  
+  
+  
+  
+  // Session persistence
+  
+  private static final String COOKIE_NAME_SESSION_ID = "JSESSIONID";
+  private static final long THRESHOLD_TO_BE_EXPIRED = 1000 * 60 * 60; // 1 hour
+  
+  private void persistSession(HttpSession session, Integer maxAgeAsSeconds) {
+    session.setMaxInactiveInterval(maxAgeAsSeconds);
+
+    Cookie cookie = createCookie(session);
+    cookie.setMaxAge(maxAgeAsSeconds);
+    this.response.addCookie(cookie);
+  }
+  
+  private Cookie createCookie(HttpSession session) {
+    Cookie cookie = new Cookie(COOKIE_NAME_SESSION_ID, session.getId());
+    cookie.setPath(this.request.getContextPath());
+    return cookie;
+  }
+  
+  private Integer getPersistentSessionMaxAge() {
+    for (Cookie cookie : this.request.getCookies()) {
+      if (cookie.getName().equals(COOKIE_NAME_SESSION_ID) && cookie.getMaxAge() > 0) {
+        return cookie.getMaxAge();
+      }
+    }
+    return null;
+  }
+  
+  private void setSessionCookieWhenPersistentCookieIsAboutToBeExpired(HttpSession session) {
+    if (isPersistentCookieAboutToBeExpired(session)) {
+      this.response.addCookie(createCookie(session));
+      logger.debug("The persistent Cookie to be overwritten with a session cookie.");
+    }
+  }
+
+  private boolean isPersistentCookieAboutToBeExpired(HttpSession session) {
+    Integer maxAge = getPersistentSessionMaxAge();
+    if (maxAge == null) return false;
+    
+    long sessionAge = DateTime.getCurrentTime().getTime() - session.getCreationTime();
+    long left = (maxAge * 1000) - sessionAge;
+    return left < THRESHOLD_TO_BE_EXPIRED;
   }
 }
