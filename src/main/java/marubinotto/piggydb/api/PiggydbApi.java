b450745f2fa1d312c508672fc0e163393b528abb
Daisuke Morita <daisuke.marubinotto@gmail.com>
2014-05-15 14:19:19 +0900

    working on a session check filter

--- src/main/java/marubinotto/piggydb/api/PiggydbApi.java
+++ src/main/java/marubinotto/piggydb/api/PiggydbApi.java
@@ -1,6 +1,7 @@
 package marubinotto.piggydb.api;
 
-import marubinotto.piggydb.ui.page.common.Session;
+import marubinotto.piggydb.model.auth.User;
+import marubinotto.piggydb.service.DomainModelBeans;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,11 +15,24 @@ public class PiggydbApi implements SparkApplication {
   
   private static Log log = LogFactory.getLog(PiggydbApi.class);
   
-  private ApplicationContext applicationContext;
-  private Session session;
+  protected ApplicationContext applicationContext;
+  protected DomainModelBeans domain;
+  
+  protected Session session;
+  protected User user;
   
   public void setApplicationContext(ApplicationContext applicationContext) {
     this.applicationContext = applicationContext;
+    this.domain = new DomainModelBeans(this.applicationContext);
+  }
+  
+  protected User autoLoginAsAnonymous() {
+    User user = this.domain.getAuthentication().authenticateAsAnonymous();
+    if (user == null) return null;
+
+    this.session.start(user, null);
+    log.debug("Anonymous session created");
+    return user;
   }
 
   @Override
@@ -26,14 +40,22 @@ public class PiggydbApi implements SparkApplication {
     before(new Filter() {
       @Override
       public void handle(Request request, Response response) {
-        // session = new Session(request.raw(), response.raw()
+        session = new Session(
+          request.raw(), 
+          response.raw(), 
+          domain.getAuthentication().isEnableAnonymous());
+        
+        user = session.getUser();
+        if (user == null) user = autoLoginAsAnonymous();
+        
+        // TODO: check the session
       }
     });
     
     get(new Route("/login") {
       @Override
       public Object handle(Request request, Response response) {
-         return "Hello World!";
+        return "Hello World!";
       }
     });
   }
