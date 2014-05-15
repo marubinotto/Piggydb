9eda3ccb383c7b699a0b53aefec899b249fd90b4
Daisuke Morita <daisuke.marubinotto@gmail.com>
2014-05-15 13:52:37 +0900

    trivial change

--- src/main/java/marubinotto/piggydb/api/PiggydbApi.java
+++ src/main/java/marubinotto/piggydb/api/PiggydbApi.java
@@ -1,5 +1,7 @@
 package marubinotto.piggydb.api;
 
+import marubinotto.piggydb.ui.page.common.Session;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +15,7 @@ public class PiggydbApi implements SparkApplication {
   private static Log log = LogFactory.getLog(PiggydbApi.class);
   
   private ApplicationContext applicationContext;
+  private Session session;
   
   public void setApplicationContext(ApplicationContext applicationContext) {
     this.applicationContext = applicationContext;
@@ -20,6 +23,13 @@ public class PiggydbApi implements SparkApplication {
 
   @Override
   public void init() {
+    before(new Filter() {
+      @Override
+      public void handle(Request request, Response response) {
+        // session = new Session(request.raw(), response.raw()
+      }
+    });
+    
     get(new Route("/login") {
       @Override
       public Object handle(Request request, Response response) {
